cf98041efd406bce417d7a435b46a17c80fe81ea
talgoa <nosfe.delacrypt@gmail.com>
2021-11-06 11:05:36 +0100

    resolved warning

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,6 +1,5 @@
 import { useMoralisCloudFunction } from "react-moralis";
 import { Route, Switch, useParams, useRouteMatch } from "react-router";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -24,11 +23,6 @@ export default function Account() {
   return (
     <div>
       <h2>Account</h2>
-
-      {/* The Topics page has its own <Switch> with more routes
-              that build on the /topics URL path. You can think of the
-              2nd <Route> here as an "index" page for all topics, or
-              the page that is shown when no topic is selected */}
       <Switch>
         <Route path={`${match.path}/:ethAddress`}>
           <SpecificAccount />
